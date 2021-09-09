6662cfbfdc18ea68998973f87b929a5ac9f42f25
noemi86-cpu <noemipatermo@gmail.com>
2021-09-09 19:31:52 +0200

    in fieri

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,9 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Movie; //in questo modo colleghi il tuo Model (Movie)
+
+
 class HomeController extends Controller
 {
     public function index() {
-        return view('welcome');
+
+        $allMovies = Movie::all();// prendi tutti gli elementi all'interno della tua tabella [SELECT * FROM movies]
+
+         return view('home', compact('allMovies'));
     }
+     
 }
+
+// dump($allMovies[0]->title);//il model MOVIE ha già compilato in automatico la classe, popolandola con le proprietà
+   
